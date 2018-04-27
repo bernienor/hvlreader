b7a22ccaad5bf52c1e4a3806f8b99bf951336d1b
bernienor <bernthustadhembre@gmail.com>
2018-04-27 09:47:52 +0200

    Initial commit!

---
+++ hvlfilereader.py
@@ -0,0 +1,25 @@
+# Samleklasse for import av datafiler inn i Python. 
+#
+# The idea is to make a general importer to easy the work involved in setting
+# up new programs for post processing of test data.
+#
+# Main attributes:
+# * data ( the data set)
+# * sample_interval
+# * timestamp_of_first_sample (default 0)
+#
+#
+# Nice to have attributes:
+# * scale (default 1)
+# * unit (default "")
+# * source_file_type (subclass of reader)
+# * file_timestamp (timestamp in the filesystem of the file)
+# * data_timestamp (time and date read from the file)
+
+
+    class hvlfilereader:
+
+    
+
+
+    data = np.loadtxt(file_name, delimiter=',', skiprows=16)
---
+++ plot_datafile.py
@@ -0,0 +1,34 @@
+# plotting av data
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+def read_datafile(file_name):
+    # the skiprows keyword is for heading, but I don't know if trailing lines
+    # can be specified
+    data = np.loadtxt(file_name, delimiter=',', skiprows=16)
+    return data
+
+
+# Lar programmet håndtere argumenter. Slik at vi enkelt kan plotte en eller mange filer.
+parser = argparse.ArgumentParser(description='Plotting files stored in CSV-format from a Tektronix scope')
+parser.add_argument('filename', metavar='Filename', type=str, nargs='+',
+                   help="The filename you'd like to plot")
+parser.add_argument('inputdata', metavar='Column', type=int,help="Column to display. (1 or higher)", default=1)                   
+parser.add_argument('separator')
+# Vi må prosessere de argumentene som er sendt med programmet
+args = parser.parse_args() 
+
+# Gå gjennom lista over filer og plot dem en etter en.
+for filename in args.filename:
+    data = read_datafile(filename)
+    fig = plt.figure()
+    plt.plot(data[:,0],data[:,args.inputdata])
+    plt.show()
+
+
+
+
+
